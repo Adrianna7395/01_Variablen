bdfc0cbeb0706516341d5f51709ae1314df7ac22
Adriana <hrl.davidson@gmail.com>
2022-01-17 09:19:18 +0100

    deklaration, wertzuweisung

--- script_01.js
+++ script_01.js
@@ -1,7 +1,18 @@
 "use strict";
 
 /***** Variablen 01 *******/
+// Zeilen Kommentar
+/** 
+ * Block Kommentar
+ */ 
 
 
+// console.log("Hello World!");
 
+let firstName;
+firstName = "Max";
+console.log(firstName);
 
+let familyName = "Mütze";
+console.log(familyName);
+console.log(firstName + " " + familyName);
