79be5eac277da54f26550fe697082ab9916e9e9b
Adriana <hrl.davidson@gmail.com>
2022-01-17 11:06:45 +0100

    Beispiel

--- script_01.js
+++ script_01.js
@@ -24,10 +24,39 @@
 // familyName = prompt("Bitte Nachnamen eingeben:");
 // console.log(firstName + " " + familyName);
 
-let test;
-test = "Hi";
-test = 2;
-test = true;
+// let test;
+// test = "Hi";
+// test = 2;
+// test = true;
 
-console.log("Datentyp: " + typeof test);
-console.log("Inhalt: " + test);
+// console.log("Datentyp: " + typeof test);
+// console.log("Inhalt: " + test);
+
+
+/****KONSTANTEN     */
+ 
+// const test = "hi"; // Variablen mit konstantem Inhalt
+// test = 2; //keine Zuweisung möglich
+// console.log("Inhalt: " + test);
+
+//Beispiel
+
+// Deklaration
+let ageJohn, ageMark;
+let birthYearJohn, birthYearMark; //birth_year_john
+
+// let year = 2022;
+let date = new Date();
+let year = date.getFullYear();
+
+
+// //WERTZUWEISUNG
+ageJohn = 25;
+ageMark = 30;
+
+// Berechnung
+birthYearJohn = year - ageJohn;
+birthYearMark = year - ageMark;
+// Ausgabe
+console.log("birthYearJohn: " + birthYearJohn);
+console.log("birthYearMark: " + birthYearMark);
